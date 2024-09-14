c2d69ef200a5164afbe9380ebd6563b58417b09c
gphilipose <ginaphilipose@gmail.com>
2024-09-13 20:25:54 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const menu = document.getElementById("key")
+var last = 0;
 
-
-
+let str = ["A", "A#", "Bb", "B", "C", "C#", "Db", "D", "D#", "Eb", "E", "F", "F#", "Gb", "G", "G#", "Ab"]
+const chordLetters = document.getElementsByClassName("transpose")
 
 
 
 window.addEventListener("load", function() {
-    menu.value = "A";
+    menu.value = "0";
+});
+
+
+menu.addEventListener("input", function(){
+    //menu.value = "C";
+    var lastLetter = str[last];
+    for (let i = 0; i < chordLetters.length; i++) {
+        let regular = new RegExp(`{$lastLetter}(?![#b])`, 'g');
+        //let current = chordLetters[i].innerHTML.replace(`{$variableValue}(?![#b])`, str[menu.value])
+        let current = chordLetters[i].innerHTML.replace(regular, str[menu.value]);
+        //current = chordLetters[i].innerHTML.replace(/A(?!#)/g, str[1]);
+        chordLetters[i].innerHTML = current;
+    }
+
+    
 });
