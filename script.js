158472887b9fc97373e354d8b3f195375b6d67bf
gphilipose <ginaphilipose@gmail.com>
2024-09-14 18:20:08 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -10,17 +10,25 @@ window.addEventListener("load", function() {
     menu.value = "0";
 });
 
-
+// fix this:
 menu.addEventListener("input", function(){
     //menu.value = "C";
     var lastLetter = str[last];
     for (let i = 0; i < chordLetters.length; i++) {
-        let regular = new RegExp(`{$lastLetter}(?![#b])`, 'g');
+        let regular = new RegExp(`(${lastLetter})(?![#b])`, 'g');
+        //let regular = new RegExp(`{$lastLetter}(?![#b])`, 'g');
         //let current = chordLetters[i].innerHTML.replace(`{$variableValue}(?![#b])`, str[menu.value])
         let current = chordLetters[i].innerHTML.replace(regular, str[menu.value]);
         //current = chordLetters[i].innerHTML.replace(/A(?!#)/g, str[1]);
+
+
+
+
+        
         chordLetters[i].innerHTML = current;
+        
     }
+    last = menu.value; 
 
     
 });
